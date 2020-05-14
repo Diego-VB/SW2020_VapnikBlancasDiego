cbc321d6e7e0cbc700baa742876b5079ea583ec3
diego vap <diego.vap@comunidad.unam.mx>
2020-05-14 15:21:29 -0500

    Feat:se creo Practica2.php

---
+++ Practica2.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Diego Vapnik Blancas";
+echo " Lo que más he aprendido del curso web es a abstraer mi pensamiento y volverlo de una manera logica para poder porcesarlo."
+echo "</br>";
+echo "Tambien las posibilidades y nuevas maneras de resolver problemas de forma ingeniosa";
+
+
+
+?>
