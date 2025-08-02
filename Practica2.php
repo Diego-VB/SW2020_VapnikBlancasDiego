99a28f6ab74858195b03921a3725eb9449bdb57b
Diego-VB <65326866+Diego-VB@users.noreply.github.com>
2025-08-01 19:22:28 -0600

    Update Practica2.php

--- Practica2.php
+++ Practica2.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Diego Vapnik Blancas";
+echo "Diego Napkin";
 echo " Lo que más he aprendido del curso web es a abstraer mi pensamiento y volverlo de una manera logica para poder porcesarlo."
 echo "</br>";
 echo "Tambien las posibilidades y nuevas maneras de resolver problemas de forma ingeniosa";
